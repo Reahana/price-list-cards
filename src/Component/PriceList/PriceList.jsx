753e91db9bc178fd6f6296da23d19d05c49d4f51
Reahana <reahana233@gmail.com>
2024-01-17 23:21:59 +0600

    price list

--- src/Component/PriceList/PriceList.jsx
+++ src/Component/PriceList/PriceList.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import PriceCard from '../PriceCard/PriceCard';
 
 const PriceList = () => {
+    const [prices, setPrices]= useState([]);
+
+    useEffect(()=>{
+        fetch('prices.json')
+        .then(res=>res.json())
+        .then(data =>setPrices(data))
+    },[])
     return (
         <div>
-            
+            <h2 className='text-6xl font-bold p-4 text-center bg-purple-200 text-purple-950'>Awesome Prices</h2>
+       {
+        prices.map(price => <PriceCard 
+        key={price.id}
+        price = {price}
+        ></PriceCard>
+            )
+       }
         </div>
     );
 };

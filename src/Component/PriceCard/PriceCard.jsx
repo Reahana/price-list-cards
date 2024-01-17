a256c36b04b4c91b08db2af21bbbd741c5ebc573
Reahana <reahana233@gmail.com>
2024-01-17 23:21:49 +0600

    price card

---
+++ src/Component/PriceCard/PriceCard.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const PriceCard = ({price}) => {
+    return (
+        <div>
+            {price.name}
+        </div>
+    );
+};
+
+export default PriceCard;

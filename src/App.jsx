0aeace864124d02a65580b3bf9cde11c659db9b4
Reahana <reahana233@gmail.com>
2024-01-17 23:21:39 +0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import './App.css'
 import Navbar from './Component/Navbar/Navbar'
+import PriceList from './Component/PriceList/PriceList'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
     <Navbar></Navbar>
     <h1 className='text-7xl text-purple-700'>Hello tailwind</h1>
-  
+    <PriceList></PriceList>
     </>
   )
 }

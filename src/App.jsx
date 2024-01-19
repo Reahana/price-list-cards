a17d4634b7ee5d8475e3b9d76615c9a334b60c17
Reahana <reahana233@gmail.com>
2024-01-19 11:04:18 +0600

    rechart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-target-blank */
 
 import './App.css'
+import Dashboard from './Component/Dashboard/Dashboard'
 import Navbar from './Component/Navbar/Navbar'
 import PriceList from './Component/PriceList/PriceList'
 
@@ -11,6 +12,7 @@ function App() {
     <Navbar></Navbar>
     <h1 className='text-7xl text-purple-700'>Hello tailwind</h1>
     <PriceList></PriceList>
+    <Dashboard></Dashboard>
     </>
   )
 }
